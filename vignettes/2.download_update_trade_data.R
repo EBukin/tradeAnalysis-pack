4c6c3600c28cc9714dae39b78bc3ade390fa8950
Eduard Bukin <edwardbukin@gmail.com>
2017-02-22 20:21:44 +0100

    Script for downloading and updating CT data.

---
+++ vignettes/2.download_update_trade_data.R
@@ -0,0 +1,54 @@
+# Script for downloading and/or updating trade data
+
+
+# Setup -------------------------------------------------------------------
+
+library(data.table)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(jsonlite)
+library(stringr)
+
+# Loading funcitons
+l_ply(str_c("R/", list.files("R/", pattern="*.R")), source)
+
+# Loading data ------------------------------------------------------------
+
+# Reporters
+ctRep <- getCTReporters()
+
+# Partners
+ctPart <- getCTPartners()
+
+# # Available data:
+ctAval <- jsonlite::fromJSON("http://comtrade.un.org/api/refs/da/bulk?parameters") %>% tbl_df()
+
+# Data in the folders ----------------------------------------------------
+
+# # Monthly all countries
+updateCTData(ctAval,
+             folder = "../data/data_raw/ct_zip/m_all/",
+             period = "MONTHLY",
+             repo = "ALL" )
+
+# Mounthly by country
+updateCTData(ctAval,
+             folder = "../data/data_raw/ct_zip/m_by_country/",
+             period = "MONTHLY",
+             repo = getCTReporters()$Reporter.Code )
+
+# # Annual all countries
+updateCTData(ctAval,
+             folder = "../data/data_raw/ct_zip/a_all/",
+             period = "ANNUAL",
+             repo = "ALL")
+
+# Annual by country
+updateCTData(ctAval, 
+             folder = "../data/data_raw/ct_zip/a_by_country/", 
+             period = "ANNUAL", 
+             repo = getCTReporters()$Reporter.Code)
+
+
+
