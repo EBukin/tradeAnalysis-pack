0e2f6f721805ff507980a7a43ef3af4717ac338a
Eduard Bukin <edwardbukin@gmail.com>
2017-02-22 21:39:18 +0100

    Another stage of drafting test script.

--- projectGeorgia/2.analize_GEO_data.R
+++ projectGeorgia/2.analize_GEO_data.R
@@ -8,6 +8,8 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 library(readr)
+devtools::install_github("wilkox/treemapify")
+library(treemapify)
 
 # Loading funcitons
 l_ply(str_c("R/", list.files("R/",pattern="*.R")), source)
@@ -22,22 +24,46 @@ load("projectGeorgia/ctGEO.Rdata")
 load("data/wtoAgFood.Rdata")
 load("data/sampleData.Rdata")
 
-# Filtering commodities which we need only
+# Creating sample test data
+# ctSample <- 
+#   ctAnGeo %>% 
+#   filter(Year >= 2010, Commodity.Code %in% wtoAgFood$Commodity.Code) 
+# save(ctSample, file ="data/sampleData.Rdata")
 
+# Filtering commodities which we need only
+ctAnGeo <-
+  ctAnGeo %>% 
+  filter(Commodity.Code %in% wtoAgFood$Commodity.Code)
+
+ctMonGeo <-
+  ctMonGeo %>% 
+  filter(Commodity.Code %in% wtoAgFoodFull$Commodity.Code)
 
 # Analysis ----------------------------------------------------------------
-
-# Creating sample test data
-ctSample <- 
-  ctAnGeo %>% 
-  filter(Year >= 2010, Commodity.Code %in% wtoAgFood$Commodity.Code) 
-save(ctSample, file ="data/sampleData.Rdata")
 
 # Define top n desetinations in each commodity, year
 ctAnGeoTop <- 
   ctAnGeo %>% 
-  filter(Trade.Flow.Code == 1, Year == 2010, Commodity.Code == "02") %>% 
-  rank_top_N_partners(5, otherEU = TRUE) 
+  rank_top_N_partners(5, otherEU = TRUE) %>% 
+  mutate(Commodity.Code = as.factor(Commodity.Code),
+         Partner.Code = as.factor(Partner.Code))
+
+
+
+treeMapCoordinates <- 
+  treemapify(
+  ctAnGeoTop %>% filter(Year == 2014, Variable == "Trade.Value..US..", Trade.Flow.Code == 1, Partner.Code != 0),
+  area = "Value",
+  fill = "Value",
+  label = "Partner.Code",
+  group = "Commodity.Code"
+)
+# print(treeMapCoordinates)
+
+
+ggplotify(treeMapCoordinates)
+
+
 
 
 ctAnGeoTop %>% 
