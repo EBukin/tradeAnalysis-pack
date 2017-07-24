2da92e058fefda7eb907d9bb4b56044d4b283055
Eduard Bukin <edwardbukin@gmail.com>
2017-07-24 18:11:46 +0200

    join_lables_rus funcitonality and MT update

---
+++ R/join_labels_rus.R
@@ -0,0 +1,87 @@
+#' Add russian lables of codes to the trade data
+#'
+#'  @param rusCountries,rusFlows Path to the Rdata file with mapping tables.
+#'  @param keepCodes Logical variable for keeping or removing the codes.
+join_labels_rus <-
+  function(data,
+           rusCountries = system.file("extdata", "rus_countries.xlsx", package = "tradeAnalysis"),
+           rusFlows = system.file("extdata", "rus_flows.xlsx", package = "tradeAnalysis"),
+           keepCodes = TRUE) {
+    require(tidyverse)
+    require(readxl)
+    
+    rusCountriesMT <-
+      readxl::read_excel(rusCountries, sheet = 1, col_names = TRUE) %>%
+      dplyr::select(Code, Name)
+    
+    rusFlows <-
+      readxl::read_excel(rusFlows, sheet = 1, col_names = TRUE)
+    
+    oldNames <- c("r",
+                  "Reporter.Code",
+                  "Partner.Code",
+                  "Trade.Flow.Code",
+                  "Qty.Unit.Code")
+    newNames <-
+      c("Reporter", "Partner", "Trade.Flow", "Unit", "Commodity")
+    
+    data <-
+      data %>%
+      select_(.dots = names(.)[!names(.) %in% newNames])
+    
+    # Reporters names
+    if ("Reporter.Code" %in% names(data)) {
+      data <- data %>%
+        dplyr::mutate(Reporter.Code = as.integer(Reporter.Code)) %>%
+        dplyr::left_join(dplyr::rename(rusCountriesMT, Reporter = Name),
+                         by = c("Reporter.Code" = "Code")) %>%
+        dplyr::mutate(Reporter = ifelse(is.na(Reporter), Reporter.Code, Reporter))
+    }
+    
+    # Reporters names
+    if ("r" %in% names(data)) {
+      data <- data %>%
+        dplyr::mutate(r = as.integer(r)) %>%
+        dplyr::left_join(dplyr::rename(rusCountriesMT, Reporter = Name),
+                         by = c("r" = "Code")) %>%
+        dplyr::mutate(Reporter = ifelse(is.na(Reporter), r, Reporter))
+    }
+    
+    # Partners names
+    if ("Partner.Code" %in% names(data)) {
+      data <- data %>%
+        dplyr::mutate(Partner.Code = as.integer(Partner.Code)) %>%
+        dplyr::left_join(dplyr::rename(rusCountriesMT, Partner = Name),
+                         by = c("Partner.Code" = "Code")) %>%
+        dplyr::mutate(Partner = ifelse(is.na(Partner), Partner.Code, Partner))
+    }
+    
+    # Trade flows names
+    if ("Trade.Flow.Code" %in% names(data)) {
+      data <- data %>%
+        mutate(Trade.Flow.Code = as.integer(Trade.Flow.Code)) %>%
+        left_join(regimes, by = "Trade.Flow.Code")
+    }
+    
+    if ("Qty.Unit.Code" %in% names(data)) {
+      data <- data %>%
+        mutate(Qty.Unit.Code = as.integer(Qty.Unit.Code)) %>%
+        left_join(units %>% select(-Unit.Description),
+                  by = "Qty.Unit.Code")
+    }
+    
+    if ("Commodity.Code" %in% names(data)) {
+      data <- data %>%
+        mutate(Commodity.Code = as.character(Commodity.Code)) %>%
+        left_join(classes, by = "Commodity.Code")
+    }
+    
+    if (!keepCodes) {
+      data <-
+        data %>%
+        select_(.dots = names(.)[!names(.) %in% oldNames])
+    }
+    
+    return(data)
+    
+  }
--- R/join_lables.R
+++ R/join_lables.R
@@ -4,92 +4,98 @@
 #'  @param keepCodes Logical variable for keeping or removing the codes.
 join_lables <-
   function(data,
+           lang = NA,
            # mappingTbls = "ctClass.Rdata",
            commAggMT = system.file("extdata", "HS_agg_names.csv", package = "tradeAnalysis"),
-           reportersExtras = system.file("extdata", "reporters_extras_names.csv", package = "tradeAnalysis"),
-           partnersExtras = system.file("extdata", "partners_extras_names.csv", package = "tradeAnalysis"),
+           countriesExtras = system.file("extdata", "countries_extras_names.csv", package = "tradeAnalysis"),
            keepCodes = TRUE) {
     require(tidyverse)
     
-    class <-
-      bind_rows(classes, read.csv(commAggMT, stringsAsFactors = FALSE))
-    
-    partners <-
-      bind_rows(partners,
-                readr::read_csv(
-                  partnersExtras,
-                  col_types = cols(Partner.Code = col_integer(),
-                                   Partner = col_character())
-                )) %>% distinct()
-    
-    reporters <-
-      bind_rows(reporters,
-                readr::read_csv(
-                  reportersExtras,
-                  col_types = cols(Reporter.Code = col_integer(),
-                                   Reporter = col_character())
-                )) %>% distinct()
-    
-    oldNames <- c("r",
-                  "Reporter.Code",
-                  "Partner.Code",
-                  "Trade.Flow.Code",
-                  "Qty.Unit.Code")
-    newNames <-
-      c("Reporter", "Partner", "Trade.Flow", "Unit", "Commodity")
-    
-    data <-
-      data %>%
-      select_(.dots = names(.)[!names(.) %in% newNames])
-    
-    # Reporters names
-    if ("Reporter.Code" %in% names(data)) {
-      data <- data %>%
-        mutate(Reporter.Code = as.integer(Reporter.Code)) %>%
-        left_join(reporters, by = "Reporter.Code")
-    }
-    
-    # Reporters names
-    if ("r" %in% names(data)) {
-      data <- data %>%
-        mutate(r = as.integer(r)) %>%
-        left_join(reporters, by = c("r" = "Reporter.Code")) %>%
-        mutate(Reporter = ifelse(is.na(Reporter, Reporter.Code, Reporter)))
-    }
-    
-    # Partners names
-    if ("Partner.Code" %in% names(data)) {
-      data <- data %>%
-        mutate(Partner.Code = as.integer(Partner.Code)) %>%
-        left_join(partners, by = "Partner.Code") %>%
-        mutate(Partner = ifelse(is.na(Partner), Partner.Code, Partner))
-    }
-    
-    # Trade flows names
-    if ("Trade.Flow.Code" %in% names(data)) {
-      data <- data %>%
-        mutate(Trade.Flow.Code = as.integer(Trade.Flow.Code)) %>%
-        left_join(regimes, by = "Trade.Flow.Code")
-    }
-    
-    if ("Qty.Unit.Code" %in% names(data)) {
-      data <- data %>%
-        mutate(Qty.Unit.Code = as.integer(Qty.Unit.Code)) %>%
-        left_join(units %>% select(-Unit.Description),
-                  by = "Qty.Unit.Code")
-    }
-    
-    if ("Commodity.Code" %in% names(data)) {
-      data <- data %>%
-        mutate(Commodity.Code = as.character(Commodity.Code)) %>%
-        left_join(classes, by = "Commodity.Code")
-    }
-    
-    if (!keepCodes) {
+    if (is.na(lang)) {
+      class <-
+        bind_rows(classes, read.csv(commAggMT, stringsAsFactors = FALSE))
+      
+      countries <-
+        bind_rows(
+          dplyr::rename(partners, Code = Partner.Code, Name = Partner),
+          dplyr::rename(reporters, Code = Reporter.Code, Name = Reporter),
+          readr::read_csv(
+            countriesExtras,
+            col_types = cols(Code = col_integer(),
+                             Name = col_character())
+          )
+        ) %>%
+        distinct()
+      
+      oldNames <- c("r",
+                    "Reporter.Code",
+                    "Partner.Code",
+                    "Trade.Flow.Code",
+                    "Qty.Unit.Code")
+      newNames <-
+        c("Reporter", "Partner", "Trade.Flow", "Unit", "Commodity")
+      
       data <-
         data %>%
-        select_(.dots = one_of(oldNames))
+        select_(.dots = names(.)[!names(.) %in% newNames])
+      
+      # Reporters names
+      if ("Reporter.Code" %in% names(data)) {
+        data <- data %>%
+          mutate(Reporter.Code = as.integer(Reporter.Code)) %>%
+          left_join(dplyr::rename(countries, Reporter = Name),
+                    by = c("Reporter.Code" = "Code")) %>%
+          mutate(Reporter = ifelse(is.na(Reporter), Reporter.Code, Reporter))
+      }
+      
+      # Reporters names
+      if ("r" %in% names(data)) {
+        data <- data %>%
+          mutate(r = as.integer(r)) %>%
+          left_join(dplyr::rename(countries, Reporter = Name),
+                    by = c("r" = "Code")) %>%
+          mutate(Reporter = ifelse(is.na(Reporter), r, Reporter))
+      }
+      
+      # Partners names
+      if ("Partner.Code" %in% names(data)) {
+        data <- data %>%
+          mutate(Partner.Code = as.integer(Partner.Code)) %>%
+          left_join(dplyr::rename(countries, Partner = Name),
+                    by = c("Partner.Code" = "Code")) %>%
+          mutate(Partner = ifelse(is.na(Partner), Partner.Code, Partner))
+      }
+      
+      # Trade flows names
+      if ("Trade.Flow.Code" %in% names(data)) {
+        data <- data %>%
+          mutate(Trade.Flow.Code = as.integer(Trade.Flow.Code)) %>%
+          left_join(regimes, by = "Trade.Flow.Code")
+      }
+      
+      if ("Qty.Unit.Code" %in% names(data)) {
+        data <- data %>%
+          mutate(Qty.Unit.Code = as.integer(Qty.Unit.Code)) %>%
+          left_join(units %>% select(-Unit.Description),
+                    by = "Qty.Unit.Code")
+      }
+      
+      if ("Commodity.Code" %in% names(data)) {
+        data <- data %>%
+          mutate(Commodity.Code = as.character(Commodity.Code)) %>%
+          left_join(classes, by = "Commodity.Code")
+      }
+      
+      if (!keepCodes) {
+        data <-
+          data %>%
+          select_(.dots = one_of(oldNames))
+      }
+      
+    } else {
+      data <- join_labels_rus(data = data, keepCodes = keepCodes)
     }
     
     return(data)
+    
   }
