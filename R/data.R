abb098a921f9adf93ca716dd66632f7c50c66e90
Eduard Bukin <edwardbukin@gmail.com>
2017-08-12 02:40:48 +0300

    Fixing mapping table and documenting data

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' All HS commodity codes available in COMTRADE
+#' 
+"classes"
+
+#' All partners available in COMTRADE
+#' 
+"partners"
+
+#' All reporters available in COMTRADE
+#' 
+"reporters"
+
+#' All trade flows available in COMTRADE
+#' 
+"regimes"
+
+#' All units descriptions available in COMTRADE
+#' 
+"units"
+
+#' All main WTO agri-food HS codes (49 in total) 
+#' 
+"wtoAgFood"
+
+#' All WTO agri-food HS codes (49 in total) 
+#' 
+"atoAgFoodFull"
